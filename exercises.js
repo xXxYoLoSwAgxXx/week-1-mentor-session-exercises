ab512309017c0c8fef60af67444b3362a121d569
lorecrafting <raymond.n.luong@gmail.com>
2017-04-12 09:50:41 -1000

    initial commit

---
+++ exercises.js
@@ -0,0 +1,41 @@
+/** Function: firstReverse
+ * The function will take the str parameter being passed in and
+ * return the string in reversed order
+ * @param {string} str
+ * @return {string} reversed
+ * ie: "cat" => "tac"
+ */
+
+ /** Function: alphaOrder
+ * The function will take the str parameter being passed in and
+ * return the string in alphabetical order
+ * @param {string} str
+ * @return {string} in alphabetical order
+ * ie: "cake" => "acek"
+ */
+
+ /** Function: vowelCount
+ * The function will take the str parameter being passed in and
+ * return the number of vowels in the string
+ * @param {string} str
+ * @return {number} count of vowels
+ * ie: "oreo" => 3
+ */
+
+ /** Function: timeConvert
+ * The function will take the str parameter being passed in and
+ * return the number of hours and minutes. Seperate the number of hours
+ * and minutes with a colon
+ * @param {number} str
+ * @return {string} as hours:minutes
+ * ie: 68 => 1:8
+ */
+
+ /** Function: repeatString
+ * The function will take in two parameters and repeat a given string (first argument)
+ * num times (second argument). Return an empty string if num is a negative number
+ * @param {string} str
+ * @param {num} times
+ * @return {string} repeated num times
+ * i.e repeatString("money", 3) => "moneymoneymoney".
+ */
