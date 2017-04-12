311d31a8dd429e913e74f77cf4a89ca01f1f561b
lorecrafting <raymond.n.luong@gmail.com>
2017-04-12 13:10:11 -1000

    cleanup

--- exercises.js
+++ exercises.js
@@ -6,14 +6,6 @@
  * ie: "cat" => "tac"
  */
 
- function firstReverse(str) {
-    if (typeof str === "string") {
-      return str.split("").reverse().join("")
-    } else {
-      return null
-    }
- }
-
  /** Function: alphaOrder
  * The function will take the str parameter being passed in and
  * return the string in alphabetical order
@@ -22,14 +14,6 @@
  * ie: "cake" => "acek"
  */
 
- function alphaOrder(str) {
-  if (typeof str === "string") {
-    return str.split("").sort().join("")
-  } else {
-    return null
-  }
- }
-
  /** Function: vowelCount
  * The function will take the str parameter being passed in and
  * return the number of vowels in the string
@@ -37,27 +21,6 @@
  * @return {number} count of vowels
  * ie: "oreo" => 3
  */
-
- function vowelCount(str) {
-  var vowelMap = {
-    a: 1,
-    e: 1,
-    i: 1,
-    o: 1,
-    u: 1
-  }
-  var count = 0;
-  if (typeof str === "string") {
-    for (var i = 0; i < str.length; i++) {
-      if (vowelMap.hasOwnProperty(str[i])) {
-        count++;
-      }
-    }
-    return count;
-  } else {
-    return null
-  }
- }
 
  /** Function: timeConvert
  * The function will take the str parameter representing the amount of minutes being passed in and
@@ -68,18 +31,6 @@
  * ie: 68 => 1:8
  */
 
- function timeConvert(num) {
-  var hours, minutes;
-  if (typeof num === "number") {
-    hours = Math.floor(num/60);
-    minutes = num % 60
-    console.log('hours', hours, 'minutes', minutes)
-    return hours + ':' + minutes
-  } else {
-    return null
-  }
- }
-
  /** Function: repeatString
  * The function will take in two parameters and repeat a given string (first argument)
  * num times (second argument). Return an empty string if num is a negative number
@@ -89,17 +40,6 @@
  * i.e repeatString("money", 3) => "moneymoneymoney".
  */
 
- function repeatString(str, num) {
-  var result = ''
-  if (typeof str === "string" && typeof num === "number") {
-    for (var i = 0; i < num; i++) {
-      result += str
-    }
-    return result
-  } else {
-    return null
-  }
- }
 
 /**
  * Below here we see a module.exports which is set to an object with a bunch of keys.
@@ -116,9 +56,9 @@
  */
 
 module.exports = {
-    firstReverse: firstReverse,
-    alphaOrder: alphaOrder,
-    vowelCount: vowelCount,
-    timeConvert: timeConvert,
-    repeatString: repeatString
+    firstReverse: null,
+    alphaOrder: null,
+    vowelCount: null,
+    timeConvert: null,
+    repeatString: null
 }
