1ad01df70feaf8ec48e4ef88bdd9202cdadd3e93
Isaiah Harris <harrisfambam@gmail.com>
2018-06-10 15:25:49 -1000

    complete week-1

--- exercises.js
+++ exercises.js
@@ -1,3 +1,4 @@
+module.exports = {
 /** Function: firstReverse
  * The function will take the str parameter being passed in and
  * return the string in reversed order
@@ -5,7 +6,16 @@
  * @return {string} reversed
  * ie: "cat" => "tac"
  */
-
+firstReverse : function(str){
+    let reverseStr = '';
+    if (typeof str === 'string'){
+        return str.split('').reverse().join('');
+    } else {
+        return null;
+    }
+    
+    return reverseStr;
+},
  /** Function: alphaOrder
  * The function will take the str parameter being passed in and
  * return the string in alphabetical order
@@ -13,7 +23,16 @@
  * @return {string} in alphabetical order
  * ie: "cake" => "acek"
  */
-
+alphaOrder : function (str){
+    let alpha = '';
+    if (typeof str === 'string'){
+        return str.split('').sort().join('');
+    } else {
+        return null;
+    }
+    
+    return alpha;
+},
  /** Function: vowelCount
  * The function will take the num parameter being passed in and
  * return the number of vowels in the string
@@ -21,6 +40,19 @@
  * @return {number} count of vowels
  * ie: "oreo" => 3
  */
+vowelCount : function (str){
+    let arr = [];
+    if(typeof str === 'string'){
+        for (let i = 0; i < str.length; i++) {
+            if(str[i].match(/[aeiouAEIOU]/)){
+                arr.push(str[i]);
+            }
+        }
+        return arr.length;
+    } else {
+        return null;
+    }
+},
 
  /** Function: timeConvert
  * The function will take the str parameter representing the amount of minutes being passed in and
@@ -30,7 +62,15 @@
  * @return {string} as hours:minutes
  * ie: 68 => 1:8
  */
-
+timeConvert : function(str){
+    if(typeof str === 'number'){
+    var min = str % 60;
+    var h = (str - min)/ 60;
+    return h + ':' + min;
+    } else {
+        return null;
+    }
+},
  /** Function: repeatString
  * The function will take in two parameters and repeat a given string (first argument)
  * num times (second argument). Return an empty string if num is a negative number
@@ -39,7 +79,14 @@
  * @return {string} repeated num times
  * i.e repeatString("money", 3) => "moneymoneymoney".
  */
-
+repeatString : function (str, num){
+    if (num > 0 ){
+        var rep = str.repeat(num);
+        return rep;
+    } else {
+        return null;
+    }
+}
 
 /**
  * Below here we see a module.exports which is set to an object with a bunch of keys.
@@ -54,11 +101,12 @@
  * the test-runner is importing this file so that it can gain access to the functions
  * and run tests on it.
  */
+}
 
-module.exports = {
-    firstReverse: null,
-    alphaOrder: null,
+/*module.exports = {
+    firstReverse: firstReverse,
+    alphaOrder: alphaOrder,
     vowelCount: null,
     timeConvert: null,
     repeatString: null
-}
+}*/
